cbc3459fa55eac40605fc09b3bed3460ec930134
tom_glb <goodMorning_glb@atguigu.com>
2019-05-02 00:29:10 +0800

    add something on master barnch

---
+++ src/main/java/com/atguigu/crud/test/MasterBranchTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.crud.test;
+
+/**
+ * @author ShawnYang
+ * @create 2019-05-02 0:27
+ */
+public class MasterBranchTest {
+    public static void main(String[] args) {
+        System.out.println("add something on the master branch ");
+    }
+}
