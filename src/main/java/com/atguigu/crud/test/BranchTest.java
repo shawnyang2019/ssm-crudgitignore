7463deb0306a4ca8c18e26f2eda0f47e4358caf6
tom_glb <goodMorning_glb@atguigu.com>
2019-05-02 00:43:40 +0800

    add something on branchtest branch

---
+++ src/main/java/com/atguigu/crud/test/BranchTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.crud.test;
+
+/**
+ * @author ShawnYang
+ * @create 2019-05-02 0:33
+ */
+public class BranchTest {
+    public static void main(String[] args) {
+        System.out.println("add something on the branch test branch ");
+
+    }
+}
