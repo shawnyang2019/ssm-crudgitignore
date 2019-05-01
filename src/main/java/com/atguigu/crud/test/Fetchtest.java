62403b4b3406a5690b36fb243edc15e39e23889b
tom_glb <goodMorning_glb@atguigu.com>
2019-05-02 02:09:09 +0800

    fetchtest

--- src/main/java/com/atguigu/crud/test/Fetchtest.java
+++ src/main/java/com/atguigu/crud/test/Fetchtest.java
@@ -1,8 +1,13 @@
 package com.atguigu.crud.test;
 
+import java.sql.SQLOutput;
+
 /**
  * @author ShawnYang
  * @create 2019-05-02 2:06
  */
 public class Fetchtest {
+    public static void main(String[] args) {
+        System.out.println("add some to test fetch");
+    }
 }
