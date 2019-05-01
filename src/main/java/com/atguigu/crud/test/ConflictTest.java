fc66e0e16db4f4b2bb1cfb841c7b2c54ee8f7697
tom_glb <goodMorning_glb@atguigu.com>
2019-05-02 01:13:12 +0800

    add another on conflict branch after create branch conflict branch

---
+++ src/main/java/com/atguigu/crud/test/ConflictTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.crud.test;
+
+/**
+ * @author ShawnYang
+ * @create 2019-05-02 1:04
+ */
+public class ConflictTest {
+    public static void main(String[] args) {
+        System.out.println("add something,then create branch conflict branch ");
+        System.out.println("add another on conflict branch after create branch conflict branch  ");
+    }
+}
